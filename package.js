0a9d9e7b15ea7f280b8d95b69dbff1ea0b31a83e
Simon <svansintjan@gmail.com>
2015-10-08 15:33:05 -0800

    Add description and github link

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Auth module for Meteor Running',
   // URL to the Git repository containing the source code for this package.
-  git: 'git@github.com:simonv3/meteor-running-auth.git',
+  git: 'https://github.com/simonv3/meteor-running-auth.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
