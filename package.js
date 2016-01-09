dece8fd12bc1e7f339574eab3c4111fe1aae236d
Simon <svansintjan@gmail.com>
2016-01-09 15:25:21 -0800

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'simonv3:meteor-running-auth',
-  version: '0.3.2',
+  version: '0.4.0',
   // Brief, one-line summary of the package.
   summary: 'Auth module for Meteor Running',
   // URL to the Git repository containing the source code for this package.
